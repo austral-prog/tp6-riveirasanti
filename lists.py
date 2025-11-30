60eeee90b8fc01aebca79b4818f5fce02da0aae4
riveirasanti <riveirasanti@gmail.com>
2025-11-29 22:45:16 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,20 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    result = []
+    for i in range(len(list_to_remove_elements)):
+        if i != 0 and i != 4 and i != 5:
+            result.append(list_to_remove_elements[i])
+    return result
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    return ['Pink'] + list_to_add_elements + ['Yellow']
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    return len(list_to_check) == 0
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1) < 3 or len(list_to_compare2) < 3:
+        return False
+    return list_to_compare1[2] == list_to_compare2[2]
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return [list_of_lists_to_modify[0][:2], list_of_lists_to_modify[1][1:4], list_of_lists_to_modify[2][-2:]]
